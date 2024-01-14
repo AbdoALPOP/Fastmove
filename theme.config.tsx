09bf202d1d68298e2b5f138ac098b3e1a454fcad
Abdelrahman Ragab <31578033+AbdoALPOP@users.noreply.github.com>
2024-01-14 04:05:42 +0200

    First Version

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Fastmove</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/AbdoALPOP/Fastmove',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: 'https://github.com/AbdoALPOP/Fastmove',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Fastmove Guide',
   },
 }
 
